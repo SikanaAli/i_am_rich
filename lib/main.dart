73f37a4a694b518135cb948b20aa3ff46499a094
ImmortalDev <34840787+SikanaAli@users.noreply.github.com>
2023-05-25 22:08:35 +0200

    learned how to change app iocn

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
         ),
         body: const Center(
           child: Image(
-            image: NetworkImage("https://images.unsplash.com/photo-1684720852626-84d28dbeabc1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=798&q=80"),
+            image: AssetImage("images/diamond.png"),
           ),
         ),
       ),
